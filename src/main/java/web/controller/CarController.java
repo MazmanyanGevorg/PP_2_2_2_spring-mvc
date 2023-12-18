8b6fe14d142d7eede7674097c612ab1e36b07e5b
Gev <129871410+Mazmanyak@users.noreply.github.com>
2023-12-18 16:28:47 +0400

    cars -> table 5. create new class HelloController.java

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,9 +3,13 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
